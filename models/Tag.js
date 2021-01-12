d690efd9e5f0a04c91259f8dc3c75f8a5f282979
Bradley Gosiengfiao <baseballbrad12@gmail.com>
2021-01-11 19:50:55 -0800

    Incorporating field elements for Tag.js.

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,24 @@
+//import Sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+//import databases connection
 const sequelize = require('../config/connection.js');
 
+//Initialize Tag model, extends from Model class.
 class Tag extends Model {}
 
+//Setting up fields an rules for Tag
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
